feb7b6e7b6176c86930006694acd70ef51410d52
gspagoni <gspagoni@outlook.com>
2017-07-19 15:04:38 +0200

    favicon

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ const port = 3000;
 
 app.set('view engine', 'ejs'); 
 app.use(express.static(__dirname + "/public"));
+// display infor image
 app.use(favicon(path.join(__dirname,'public','image','favicon.ico')));
 app.use(fileUpload());
 
